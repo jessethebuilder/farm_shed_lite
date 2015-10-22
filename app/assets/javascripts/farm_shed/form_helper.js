65dc85169d75c8a8cb18fb936a66762725484417
Jesse Farmer <jessethebuilder@yahoo.com>
2015-10-22 13:50:23 -0700

    x

--- app/assets/javascripts/farm_shed/form_helper.js
+++ app/assets/javascripts/farm_shed/form_helper.js
@@ -4,14 +4,7 @@ function addAssociatedFields(selector, association, content){
 	$(selector).prepend(content.replace(regex, new_id));
 }
 
-function updateSelectFromSelect(primary_select, secondary_select, data_attribute){
-	// data_attribute is the data attribute that you associate with the values in the primary select
-	$(primary_select).on('change', function(){
-		$(secondary_select).html('').append($('option[data-' + data_attribute + '="' + $(this).val() + '"]').clone());
-	});
-	
-	$(secondary_select).html('').append($('option[data-' + data_attribute + '="' + $(primary_select).val() + '"]').clone());
-}
+
 
 
 

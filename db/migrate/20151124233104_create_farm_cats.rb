fb02eca35d2fe8651cb8e0c85e7d28e2ea04bbe0
Jesse Farmer <jessethebuilder@yahoo.com>
2015-11-24 16:14:16 -0800

    FarmCat Master Class

--- db/migrate/20151124233104_create_farm_cats.rb
+++ db/migrate/20151124233104_create_farm_cats.rb
@@ -8,6 +8,7 @@ class CreateFarmCats < ActiveRecord::Migration
       t.string :slug
       t.string :has_farm_cats_type
       t.integer :has_farm_cats_id
+      t.string :master_class
 
       t.timestamps null: false
     end

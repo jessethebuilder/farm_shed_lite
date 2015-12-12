58762a7c4bf694493892d18e5db2e2364506ec57
Jesse Farmer <jessethebuilder@yahoo.com>
2015-12-11 21:12:29 -0800

    updates

--- app/helpers/schema_dot_org_helper.rb
+++ app/helpers/schema_dot_org_helper.rb
@@ -10,4 +10,13 @@ module SchemaDotOrgHelper
   def wordy_date(datetime)
     datetime.strftime('%B %e, %Y')
   end
+  
+  def machine_time(datetime)
+    datetime.strftime('%Y-%m-%d')
+  end
+  
+  def wordy_time(datetime)
+    datetime.strftime('%H:%M')
+  end
+  
 end

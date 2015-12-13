580dc49394cce487c028dd5a52f6e04565275621
Jesse Farmer <jessethebuilder@yahoo.com>
2015-12-12 18:58:43 -0800

    element in view

--- app/assets/javascripts/farm_shed/utilities.js
+++ app/assets/javascripts/farm_shed/utilities.js
@@ -13,3 +13,16 @@ function removeQueryString(url){
 function toTitleCase(str){
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
+
+function isScrolledIntoView(elem){
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}

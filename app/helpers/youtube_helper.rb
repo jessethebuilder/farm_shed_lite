250e856b1766f53d7d0cfa2fe14a7172e1919432
Jesse Farmer <jessethebuilder@yahoo.com>
2016-06-01 21:21:06 -0700

    preview_image yuotube

--- app/helpers/youtube_helper.rb
+++ app/helpers/youtube_helper.rb
@@ -1,6 +1,16 @@
 module YoutubeHelper
-    def embedded_video_from_watch(watch_url, width: '100%', frameborder: 0)
-    slug = watch_url.split('watch?v=')[1]
+  def extract_id_from_watch(url)
+    url.split('watch?v=')[1]
+  end  
+  
+  def embedded_video_from_watch(watch_url, width: '100%', frameborder: 0)
+    slug = extract_id_from_watch(watch_url) 
     %Q|<iframe width="#{width}" src="https://www.youtube.com/embed/#{slug}" frameborder="#{frameborder.to_s}" allowfullscreen></iframe>|.html_safe
   end
+  
+  def preview_image_from_watch(watch_url)
+    slug = extract_id_from_watch(watch_url)
+    "http://img.youtube.com/vi/#{slug}/0.jpg"
+  end
+  
 end
